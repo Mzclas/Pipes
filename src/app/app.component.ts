66884cd1f237ecf40945b6572a6fec6f01e6ca00
mzclas <Mzclas@gmail.com>
2019-12-25 18:07:17 +0100

    Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'pipes';
+  name = "Eugenio";
+  arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  PI = Math.PI;
+  salary = 1600;
+  game = {
+    name: "The legend of Zelda",
+    age: 1988,
+    company: "Nintendo"
+  };
 }
